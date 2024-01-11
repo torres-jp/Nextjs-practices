ca15d88d57e59f62c36f58ab5c350ae41869db45
pnauj <torres.jpw@gmail.com>
2024-01-10 22:43:37 -0300

    se comenzo el proyecto desde 0

---
+++ next-auth-radix/src/app/auth/login/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@radix-ui/themes";
+import { BookmarkFilledIcon } from "@radix-ui/react-icons";
+
+function LoginPage() {
+  return (
+    <div>
+      LoginPage
+      <Button>
+        <BookmarkFilledIcon width="16" height="16" /> login
+      </Button>
+    </div>
+  );
+}
+
+export default LoginPage;
