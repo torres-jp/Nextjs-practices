7a329358a79fb571b24ccc0a142507e4c0ee7571
pnauj <torres.jpw@gmail.com>
2024-01-15 21:38:19 -0300

    project user added

--- next-auth-radix/src/app/api/auth/[...nextauth]/route.ts
+++ next-auth-radix/src/app/api/auth/[...nextauth]/route.ts
@@ -1,58 +1,68 @@
 import NextAuth, { AuthOptions } from "next-auth";
-import  CredentialsProvider  from "next-auth/providers/credentials";
+import CredentialsProvider from "next-auth/providers/credentials";
 import prisma from "@/libs/prisma";
 import bcrypt from "bcrypt";
 
-export const authOptions: AuthOptions = {  
-    providers: [
-        CredentialsProvider({
-            name: "credentials",
-            credentials: {
-                email: { label: "Email", type: "email", placeholder: "email@example.com" },
-                password: { label: "Password", type: "password", placeholder: "******" }
-            },
-            async authorize(credentials: any, req) {
-                const { email, password } = credentials;
-
-                const userFound = await prisma.user.findUnique({
-                    where: {
-                        email
-                    }
-                })
-
-                if(!userFound) throw new Error('Invalidate credentianls')
-
-                const validatePassword = await bcrypt.compare(password, userFound.password)
-                if(!validatePassword) throw new Error('Invalid credentials')
-                return {
-                    id: userFound.id + '',
-                    name: userFound.name,
-                    email: userFound.email,
-                    
-                }
-            }
-        })
-    ],
-    callbacks: {
-        async jwt({ token, user }) {
-            if(user) {
-                token.id = user.id
-            }
-            
-            return token    
+export const authOptions: AuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "email@example.com",
         },
-        async session({session , user , token}) {
-            if(token) {
-                session.id = token.sub
-            }
-            return session
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "******",
         },
+      },
+      async authorize(credentials: any, req) {
+        const { email, password } = credentials;
+
+        const userFound = await prisma.user.findUnique({
+          where: {
+            email,
+          },
+        });
+
+        if (!userFound) throw new Error("Invalidate credentianls");
+
+        const validatePassword = await bcrypt.compare(
+          password,
+          userFound.password
+        );
+        if (!validatePassword) throw new Error("Invalid credentials");
+        return {
+          id: userFound.id + "",
+          name: userFound.name,
+          email: userFound.email,
+        };
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+
+      return token;
     },
-    pages: {
-        signIn: "/auth/login"
-    }
-}
+    async session({ session, user, token }) {
+      if (token) {
+        session.user.id = token.sub as string;
+      }
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/auth/login",
+  },
+};
 
-const handler = NextAuth(authOptions)
+const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
--- next-auth-radix/src/app/api/projects/route.ts
+++ next-auth-radix/src/app/api/projects/route.ts
@@ -6,16 +6,29 @@ import { authOptions } from "../auth/[...nextauth]/route";
 export async function POST(req: Request) {
   const data = await req.json();
 
-  const session = await getServerSession(authOptions)
-  console.log(session)
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return NextResponse.json(
+      { message: "Unauthorized" },
+      {
+        status: 401,
+      }
+    );
+  }
 
-  const newProject = await prisma.project.create({ 
+  const newProject = await prisma.project.create({
     data: {
-        title: data.title,
-        description: data.description
-    } });
+      title: data.title,
+      description: data.description,
+      user: {
+        connect: {
+          id: parseInt(session?.user.id),
+        },
+      },
+    },
+  });
 
-  return NextResponse.json(newProject,{
+  return NextResponse.json(newProject, {
     status: 201,
   });
 }
